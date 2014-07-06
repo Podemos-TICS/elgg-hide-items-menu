5f16229e463bee03bd423c5993cff576e04ed4e7
jonabasque <jonmultimedia@gmail.com>
2014-07-06 02:54:20 +0200

    Eliminación de archivos y casi finalizadas las settigns

--- start.php
+++ start.php
@@ -13,21 +13,28 @@ register_elgg_event_handler('init','system','hide_items_menu_init');
 /*METODO DE ARRANQUE DEL PLUGIN CUANDO SE DISPARA EL EVENTO INIT (CONTROLADOR ESPECIFICO)*/
 function hide_items_menu_init() {
 
+			$menuitems = get_register("menu");
+
+			if (!elgg_is_admin_logged_in()){
+
+					// Ejecutamos este metodo que desactiva una función callback que era hook de un plugin.
+					elgg_unregister_plugin_hook_handler('prepare', 'menu:site', 'elgg_site_menu_setup');
+					// El primer parámetro es el nombre del hook, el segundo es el tipo de entidad y tercero la función callback de la cual lo eliminamos.
+
+					// Para los no admins se quitan del menú los items que hemos seleccionado en settings.
+					foreach ($menuitems as $item){
+						$value_item = elgg_get_plugin_setting($item, $plugin_name = "hide-items-menu");
+						if($value_item == "yes"){
+							elgg_unregister_menu_item('site', $value_item);
+						}
+				 }
 			//variable que guardará el retorno de la función que recoge la configuración  del plugin, utilizando como parámetros el objeto "menu" y el
 			//nombre del plugin (devuelve valores como "only_admin" o "restricted", siempre relacionados con permisos de acceso)
-			$menuSettings = elgg_get_plugin_setting("menu", $plugin_name = "hide_items_menu");
-
+			$menuSettings = elgg_get_plugin_setting("Activity", $plugin_name = "hide-items-menu");
+			var_dump($menuSettings);
 			// Si el admin no está logeado y la opción en settings es only_admin....
-		 	if (!elgg_is_admin_logged_in()){
 
-						// Ejecutamos este metodo que desactiva una función callback que era hook de un plugin.
-						elgg_unregister_plugin_hook_handler('prepare', 'menu:site', 'elgg_site_menu_setup');
-						// El primer parámetro es el nombre del hook, el segundo es el tipo de entidad y tercero la función callback de la cual lo eliminamos.
-
-		         // Para los no admins se quitan del menú los items que hemos seleccionado en settings.
-						elgg_unregister_menu_item('site','members');
-						elgg_unregister_menu_item('site','bookmarks');
-		  			// la siguiente variable recoge el retorno de la función que recoge la configuración del plugin
+						// la siguiente variable recoge el retorno de la función que recoge la configuración del plugin
 		  			//$restrictedAccess = elgg_get_plugin_setting("restricted_access", $plugin_name = "hide_items_menu");
 		  			// Una vez que se ha eliminado la opción del menú, se intentará quitar del registro la página relacionada.
 		  			//if ($restrictedAccess == "restricted") {
--- views/default/items-menu.php
+++ views/default/items-menu.php
@@ -1,29 +1,5 @@
 <?php
   echo "dentro del archivo items-menu";
   echo "<br />";
-  //RECORREMOS EL ARRAY DEL MENU SITE-->
-  $menuitems = get_register('menu');
-  var_dump($menuitems);
-  //echo elgg_view("items-menu");
-  echo "<br />";
-
-  echo "<p><b>Selecciona los elementos que no quieres que se muestren:</b></p>";
-
-  foreach ($menuitems as $item){
-  //echo $item->name;
-  echo "<span>".$item->name."</span>";
-  //Por cada item imprimimos un checkbox con el valor de su nombre, y si ya existia en el array $vars[entity] imprimimos un checked...
-  /*echo "<input type='checkbox' value='".$item->name."'"
-  .if (isset($vars['entity']->item->name)) echo "checked='checked'"." name='params[".$item->name."]'/>";
-  echo " |  ";
-  */
-
-   echo "<input type='checkbox' value='".$item->name." name='params[".$item->name."]'/>";
-   echo " |  ";
-
-  if ($vars['entity']->item->name == "activity"){
-    echo "existe ;  ";
-
-  }else{echo"no existe ;  ";} // muestra no existe en todos por que todabía no se ha guardado en $vars
-}
+ echo $vars['entity']->item->name;
 ?>
